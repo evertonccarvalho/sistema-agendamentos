65613baafd19d2e68e8acb9dde558dfc90f8b3ba
Éverton Carvalho <evertonsnake@gmail.com>
2024-03-19 13:05:11 -0300

    feat: headerDashboard

---
+++ src/components/HeaderDashboard.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./ui/button";
+import { UserNav } from "./user-nav";
+
+const HeaderDashboard = () => {
+  return (
+    <header className="bg-transparent z-10 flex items-center justify-center w-full h-[72px] py-2 px-0">
+      <div className="max-w-screen-2xl  w-full py-0 pr-8 mx-2 flex  items-center justify-end">
+        <div className=" flex gap-2 items-center">
+          <Button>Convidar</Button>
+          <UserNav />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default HeaderDashboard;
---
+++ src/components/user-nav.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { useSession, signOut } from 'next-auth/react';
+import Link from 'next/link';
+
+export function UserNav() {
+	const { data, status } = useSession();
+
+	if (data?.user) {
+		return (
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button variant="ghost" className="relative h-8 w-8 rounded-full">
+						<Avatar className="h-8 w-8">
+							<AvatarImage src={data.user.image ?? ''} alt={data.user.name ?? ''} />
+							<AvatarFallback className="uppercase">
+								{data.user && data.user.name ? data.user.name[0] : ''}
+							</AvatarFallback>
+						</Avatar>
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className="w-56" align="end" forceMount>
+					<DropdownMenuLabel className="font-normal">
+						<div className="flex flex-col space-y-1">
+							<p className="text-sm font-medium leading-none">{data.user.name}</p>
+							<p className="text-xs leading-none text-muted-foreground">
+								{data.user.email}
+							</p>
+						</div>
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuGroup>
+						<Link href="/profile">
+							<DropdownMenuItem>
+								Profile
+								<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+							</DropdownMenuItem>
+						</Link>
+
+						<DropdownMenuItem>
+							Settings
+							<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+						</DropdownMenuItem>
+						<DropdownMenuItem>New Team</DropdownMenuItem>
+					</DropdownMenuGroup>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem onClick={() => signOut()}>
+						Log out
+						<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		);
+	} else {
+		return null; // ou retorne um componente de carregamento aqui se desejar
+	}
+}
