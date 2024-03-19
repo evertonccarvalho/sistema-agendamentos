30eec4b17b5d14cab60024a8b957df47f785d3b8
Éverton Carvalho <evertonsnake@gmail.com>
2024-03-19 00:02:51 -0300

    feat: nextAuth

--- src/app/(pages)/(auth)/login/user-auth-form.tsx
+++ src/app/(pages)/(auth)/login/user-auth-form.tsx
@@ -8,26 +8,53 @@ import { Loader2 } from "lucide-react"
 import { signIn } from "next-auth/react"
 import { FcGoogle } from "react-icons/fc"
 import { FaGithub } from "react-icons/fa"
-
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useForm } from "react-hook-form"
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false)
 
-  async function onSubmit(event: React.SyntheticEvent) {
-    event.preventDefault()
-    setIsLoading(true)
+  const LoginSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, { message: 'mínimo 8 caracteres' }), // Mínimo 8 caracteres para a senha
+  });
 
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
-  }
-  const handleLoginCLick = async () => {
+  type FormValues = z.infer<typeof LoginSchema>;
+  const form = useForm<FormValues>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleGoogleClick = async () => {
     await signIn('google');
   };
+
+  const handleGithubClick = async () => {
+    await signIn('github');
+  };
+
+
+  async function onSubmit(data: z.infer<typeof LoginSchema>) {
+    setIsLoading(true)
+    const res = await signIn('credentials', {
+      email: data.email,
+      password: data.password,
+      redirect: true,
+      callbackUrl: '/dashboard'
+    });
+    console.log('loginres', res);
+  }
+
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={onSubmit}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
@@ -64,7 +91,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
       <div className="flex gap-2 w-full">
 
         <Button
-          onClick={handleLoginCLick}
+          onClick={handleGoogleClick}
           className="w-full"
           variant="outline" type="button" disabled={isLoading}>
           {isLoading ? (
@@ -75,7 +102,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
           Google
         </Button>
         <Button
-          onClick={handleLoginCLick}
+          onClick={handleGithubClick}
           className="w-full"
 
           variant="default" type="button" disabled={isLoading}>
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import NextAuth, { AuthOptions } from 'next-auth';
 import { Adapter } from 'next-auth/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 import { db } from './prisma';
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
 	adapter: PrismaAdapter(db) as Adapter,
@@ -11,12 +12,40 @@ export const authOptions: AuthOptions = {
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
+		CredentialsProvider({
+			// The name to display on the sign in form (e.g. "Sign in with...")
+			name: "Credentials",
+			// `credentials` is used to generate a form on the sign in page.
+			// You can specify which fields should be submitted, by adding keys to the `credentials` object.
+			// e.g. domain, username, password, 2FA token, etc.
+			// You can pass any HTML attribute to the <input> tag through the object.
+			credentials: {
+				email: { label: 'email', type: 'text' },
+				password: { label: 'password', type: 'password' }
+			},
+			async authorize(credentials, req) {
+				const response = await fetch('http://localhost:3000/login', {
+					method: 'POST',
+					headers: {
+						'Content-type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: credentials?.email,
+						password: credentials?.password
+					})
+				})
+
+				const user = await response.json()
+
+				if (user && response.ok) {
+					return user
+				}
+
+				return null
+			}
+
+		})
 	],
-	pages: {
-		signIn: '/login',
-		newUser: '/register',
-		signOut: '/',
-	},
 	callbacks: {
 		async session({ session, user }) {
 			session.user = { ...session.user, id: user.id } as {
@@ -26,6 +55,11 @@ export const authOptions: AuthOptions = {
 			};
 			return session;
 		},
+	},
+	pages: {
+		signIn: '/login',
+		newUser: '/register',
+		signOut: '/',
 	},
 	secret: process.env.NEXT_AUTH_SECRET,
 };
