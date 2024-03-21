508b75a44b79fc6683b03709654d7c27e6caccd9
Éverton Carvalho <evertonsnake@gmail.com>
2024-03-20 21:56:03 -0300

    chore: calendar shadcn flex w-full

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -20,8 +20,8 @@ function Calendar({
 			showOutsideDays={showOutsideDays}
 			className={cn("p-3", className)}
 			classNames={{
-				months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-				month: "space-y-4",
+				months: "flex w-full flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
+				month: "space-y-4 flex w-full flex-col ",
 				caption: "flex justify-center pt-1 relative items-center",
 				caption_label: "text-sm font-medium",
 				nav: "space-x-1 flex items-center",
@@ -34,7 +34,7 @@ function Calendar({
 				table: "w-full border-collapse space-y-1",
 				head_row: "flex",
 				head_cell:
-					"text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+					"text-muted-foreground rounded-md w-full font-normal text-[0.8rem]",
 				row: "flex w-full mt-2",
 				cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
 				day: cn(
