e0860f4e7d72c00760a685912490a41c3e4d00d6
JsCodeDevlopment <jonatasilva118@gmail.com>
2024-03-26 13:13:42 -0300

    feat: add toast at crud event

--- src/actions/eventType/saveEvent.ts
+++ src/actions/eventType/saveEvent.ts
@@ -17,10 +17,7 @@ interface CreateEventParams {
 
 export const createEvent = async (params: CreateEventParams) => {
 	try {
-		console.log("Tentativa de criar evento:", params); // Adiciona registro para verificar os parâmetros recebidos
-		const capacity = params.capacity ? Number(params.capacity) : null; // Convertendo para número ou mantendo como null se não for fornecido
-
-		await db.eventType.create({
+		const create = await db.eventType.create({
 			data: {
 				name: params.name,
 				description: params.description,
@@ -29,14 +26,12 @@ export const createEvent = async (params: CreateEventParams) => {
 				address: params.address,
 				arrivalInfo: params.arrivalInfo,
 				capacity: Number(params.capacity),
-				duration: Number(params.duration)
+				duration: Number(params.duration),
 			},
 		});
-
 		revalidatePath("/dashboard");
-		console.log("Evento criado com sucesso!"); // Adiciona registro para verificar se a criação foi bem-sucedida
+		return create;
 	} catch (error) {
-		console.error("Erro ao criar evento:", error); // Adiciona registro para verificar se ocorreu algum erro
 		if (error instanceof Error) {
 			throw new Error(`Erro ao criar o evento: ${error.message}`);
 		}
