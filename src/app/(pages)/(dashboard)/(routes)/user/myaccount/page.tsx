d41d87939f7d6905912f64b18686e5d8479fcefb
Éverton Carvalho <evertonsnake@gmail.com>
2024-03-27 21:37:09 -0300

    profile page

--- src/app/(pages)/(dashboard)/(routes)/user/myaccount/page.tsx
+++ src/app/(pages)/(dashboard)/(routes)/user/myaccount/page.tsx
@@ -2,9 +2,9 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { MailIcon } from "lucide-react";
 import { auth } from "../../../../../../../auth";
-import { ModeToggle } from "@/components/theme-toggle";
 import { SubscriptionButton } from "../_components/subscription-button";
 import { checkSubscription } from "@/actions/subscription/subscription";
+import { ModeToggle } from "@/components/theme-toggle";
 
 const SettingsProfilePage = async () => {
   const session = await auth();
@@ -17,27 +17,7 @@ const SettingsProfilePage = async () => {
   return (
     <>
       <div className="max-w-screen-2xl mx-auto p-1 md:p-4 rounded-sm bg-muted gap-4 flex flex-col w-full h-full ">
-        <section className="flex w-full flex-col md:flex-row gap-2">
-          <div className="w-full  flex">
-            <div className="flex gap-1 items-center  p-8 h-full  w-full justify-center bg-card rounded-md">
-              <div className="flex h-full   flex-col md:flex-row items-center gap-4 justify-center">
-                <div className="px-4 lg:px-8 space-y-4">
-                  <div className="text-muted-foreground text-sm">
-                    {isPro
-                      ? "Você está atualmente em um plano Pro."
-                      : "Você está atualmente em um plano gratuito."}
-                  </div>
-                  <SubscriptionButton isPro={isPro} />
-                </div>
-                <div className="px-4 lg:px-8 py-4 space-y-4 ">
-                  <div className="flex items-center  gap-4">
-                    <p className="text-sm">Mudar Tema</p>
-                    <ModeToggle />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <section className="flex w-full  flex-col lg:flex-row gap-2">
           <div className="w-full  flex">
             <div className="flex gap-1 items-center  p-8 h-full  w-full justify-center bg-card rounded-md">
               <div className="flex h-full flex-col md:flex-row items-center gap-4 justify-center">
@@ -45,7 +25,7 @@ const SettingsProfilePage = async () => {
                   variant="ghost"
                   className=" h-14 w-14 md:h-28 md:w-28 rounded-full"
                 >
-                  <Avatar className="h-14 w-14 lg:h-28 lg:w-28">
+                  <Avatar className="h-14 w-14 md:h-28 md:w-28">
                     <AvatarImage
                       src={session.user.image ?? ""}
                       alt={session.user.name ?? ""}
@@ -56,23 +36,39 @@ const SettingsProfilePage = async () => {
                   </Avatar>
                 </Button>
 
-                <div className="flex  gap-2 items-center">
+                <div className="flex  gap-4 items-center">
                   <div>
-                    <h1 className="font-bold text-xl">
+                    <h1 className="font-bold md:text-xl">
                       Bem-Vindo, {session.user.name}
                     </h1>
-                    <div className=" flex gap-1 items-center">
+                    <div className=" flex justify-center md:justify-start gap-1 items-center">
                       <MailIcon size={18} className="text-primary" />
                       <p>{session.user.email}</p>
                     </div>
                   </div>
-                  {/* <div className="flex cursor-pointer ">
-                    <Settings size={30} className="text-primary " />
-                  </div> */}
+
+                  <ModeToggle />
+
+
                 </div>
               </div>
             </div>
           </div>
+          <div className="w-full  flex">
+            <div className="flex gap-1 items-center  p-8 h-full  w-full justify-center bg-card rounded-md">
+              <div className="flex h-full   flex-col md:flex-row items-center gap-4 justify-center">
+                <div className="px-4 flex items-center flex-col gap-4  ">
+                  <div className="text-muted-foreground  text-center text-sm">
+                    {isPro
+                      ? "Você está atualmente em um plano Pro."
+                      : "Você está atualmente em um plano gratuito."}
+                  </div>
+                  <SubscriptionButton isPro={isPro} />
+                </div>
+              </div>
+            </div>
+          </div>
+
         </section>
       </div>
     </>
