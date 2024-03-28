641650ece7685c1fdc585b00834defe0b91c23cd
Éverton Carvalho <evertonsnake@gmail.com>
2024-03-27 22:00:45 -0300

    chore: sidebar

--- src/components/sideBarLinks.tsx
+++ src/components/sideBarLinks.tsx
@@ -1,7 +1,7 @@
+import { cn } from "@/lib/utils";
 import { CalendarDays, HomeIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
 
 interface props {
   sidebarOpen: boolean;
@@ -9,32 +9,26 @@ interface props {
 const menus = [
   { name: "Inicio", link: "/dashboard", icon: HomeIcon },
   { name: "Eventos Agendados", link: "/scheduledevents", icon: CalendarDays },
-  { name: "Disponibilidade", link: "/availability", icon: CalendarDays, margin: true, },
+  {
+    name: "Disponibilidade",
+    link: "/availability",
+    icon: CalendarDays,
+    margin: true,
+  },
 ];
 
 export default function SideBarLinks({ sidebarOpen }: props) {
   const pathname = usePathname();
 
   return (
-    <div className="relative gap-10">
+    <div className="relative  gap-10">
       {menus?.map((menu) => (
-        <Link
-          href={menu.link}
-          key={menu.link}
-          className={`group py-2 flex items-center text-sm gap-3 font-medium px-1.5 hover:bg-card-foreground hover:text-secondary rounded-md ${menu.link.trim() === pathname.trim() ? "text-primary" : ""
-            }`}
-        >
-          <div>{React.createElement(menu.icon, { size: "18" })}</div>
-          <h2
-            className={`whitespace-pre duration-500 ${!sidebarOpen ? "opacity-0 hidden translate-x-28 overflow-hidden" : ""
-              }`}
-          >
+        <Link href={menu.link} key={menu.link} className={cn('group py-2 flex items-center mt-2 text-sm gap-3 font-medium px-1.5 hover:text-primary hover:bg-secondary rounded-md', pathname === menu.link ? 'text-primary bg-secondary' : 'text-muted-foreground')}        >
+          <menu.icon className={cn('h-22 w-22 ')} />
+          <h2 className={`whitespace-pre duration-500 ${!sidebarOpen ? "opacity-0 hidden translate-x-28 overflow-hidden" : ""}`}>
             {menu.name}
           </h2>
-          <h2
-            className={`${sidebarOpen ? "hidden" : ""
-              } absolute left-48 w-0 overflow-hidden whitespace-pre rounded-md bg-card px-0 py-0 font-semibold text-primary drop-shadow-lg group-hover:left-12 group-hover:w-fit group-hover:px-2 group-hover:py-1`}
-          >
+          <h2 className={cn(`${sidebarOpen ? "hidden" : ""} absolute left-48 w-0 overflow-hidden whitespace-pre rounded-md bg-card px-0 py-0 font-semibold drop-shadow-lg group-hover:left-12 group-hover:w-fit group-hover:px-2 group-hover:py-1`, { 'text-primary bg-secondary': pathname === menu.link, 'text-muted-foreground': pathname !== menu.link })}>
             {menu.name}
           </h2>
         </Link>
@@ -42,4 +36,3 @@ export default function SideBarLinks({ sidebarOpen }: props) {
     </div>
   );
 }
-
