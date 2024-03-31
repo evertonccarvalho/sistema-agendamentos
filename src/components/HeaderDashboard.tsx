2ce2b7ac3d76b7653aa54814a9c35bec77a87f60
JsCodeDevlopment <jonatasilva118@gmail.com>
2024-03-31 17:48:51 -0300

    fix header z-index

--- src/components/HeaderDashboard.tsx
+++ src/components/HeaderDashboard.tsx
@@ -2,7 +2,7 @@ import { UserNav } from "./user-nav";
 
 const HeaderDashboard = () => {
 	return (
-		<header className="bg-white dark:border-b-slate-700 dark:bg-background  border-b-[1px] absolute top-0 left-0 right-0 z-0 flex items-center justify-center w-full h-[72px] py-2 px-0">
+		<header className="bg-white dark:border-b-slate-700 dark:bg-background  border-b-[1px] absolute top-0 left-0 right-0 z-10 flex items-center justify-center w-full h-[72px] py-2 px-0">
 			<div className="container h-14 px-4 w-screen flex   py-0 pr-8 mx-2   items-center justify-end">
 				<UserNav />
 			</div>
