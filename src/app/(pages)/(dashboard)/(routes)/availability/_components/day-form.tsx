cb2845c124f47dcfe205b5c50089621840fa94ea
evertonccarvalho <evertonsnake@gmail.com>
2025-01-18 13:41:00 -0300

    feat: atualizar o DayForm para usar zod para validação e melhorar a estrutura do código

--- src/app/(pages)/(dashboard)/(routes)/availability/_components/day-form.tsx
+++ src/app/(pages)/(dashboard)/(routes)/availability/_components/day-form.tsx
@@ -1,19 +1,21 @@
-"use client";
-import { createAvailability } from "@/actions/availability/create";
-import { DayAvailabilityModel } from "@/actions/availability/getAvailabilitys";
-import { GenericForm } from "@/components/form/form";
-import { SwitchForm } from "@/components/form/SwitchForm";
-import { Button } from "@/components/ui/button";
+'use client';
+import { createAvailability } from '@/actions/availability/create';
+import { DayAvailabilityModel } from '@/actions/availability/getAvailabilitys';
+import { GenericForm } from '@/components/form/form';
+import { SwitchForm } from '@/components/form/SwitchForm';
+import { Button } from '@/components/ui/button';
 import {
 	convertMinutesToTimeString,
 	convertTimeStringToNumber,
-} from "@/utils/convertTimeStringToNumber";
-import { Check, Loader2 } from "lucide-react";
-import { useTransition } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import AddNewIntervalForm from "./add-new-interval.form";
-import TimeInterval from "./time-interval";
+} from '@/utils/convertTimeStringToNumber';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Check, Loader2 } from 'lucide-react';
+import { useTransition } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'sonner';
+import { DayFormDto, dayFormSchema } from '../schemas/day-form.schema';
+import AddNewIntervalForm from './add-new-interval.form';
+import TimeInterval from './time-interval';
 
 interface DayFormProps {
 	day: DayAvailabilityModel;
@@ -23,7 +25,8 @@ interface DayFormProps {
 const DayForm = ({ day, weekDayLabel }: DayFormProps) => {
 	const [isPending, startTransition] = useTransition();
 
-	const form = useForm({
+	const form = useForm<DayFormDto>({
+		resolver: zodResolver(dayFormSchema),
 		defaultValues: {
 			id: day.id,
 			weekDay: day.weekDay,
@@ -36,24 +39,34 @@ const DayForm = ({ day, weekDayLabel }: DayFormProps) => {
 		},
 	});
 
-	const onSubmit = async (data: any) => {
+	const onSubmit = async (data: DayFormDto) => {
+		const parsedData = dayFormSchema.parse(data);
+
 		startTransition(async () => {
 			try {
-				for (const interval of data.intervals) {
+				for (const interval of parsedData.intervals) {
 					await createAvailability({
-						weekDay: data.weekDay,
+						weekDay: parsedData.weekDay,
 						availabilityInterval: {
 							id: interval.id,
-							startTime: convertTimeStringToNumber(interval.startTime),
-							endTime: convertTimeStringToNumber(interval.endTime),
+							startTime: convertTimeStringToNumber(
+								interval.startTime
+							),
+							endTime: convertTimeStringToNumber(
+								interval.endTime
+							),
 						},
-						enabled: data.enabled,
+						enabled: parsedData.enabled,
 					});
 				}
-				toast.success(`Disponibilidade de ${weekDayLabel} salva com sucesso!`);
+				toast.success(
+					`Disponibilidade de ${weekDayLabel} salva com sucesso!`
+				);
 			} catch (error) {
-				console.error("Erro ao salvar disponibilidades:", error);
-				toast.error(`Erro ao salvar disponibilidade de ${weekDayLabel}!`);
+				console.error('Erro ao salvar disponibilidades:', error);
+				toast.error(
+					`Erro ao salvar disponibilidade de ${weekDayLabel}!`
+				);
 			}
 		});
 	};
@@ -61,7 +74,7 @@ const DayForm = ({ day, weekDayLabel }: DayFormProps) => {
 	return (
 		<GenericForm
 			form={form}
-			action={"Salvar"}
+			action={'Salvar'}
 			onSubmit={onSubmit}
 			loading={isPending}
 			hasCustomButton
@@ -70,14 +83,18 @@ const DayForm = ({ day, weekDayLabel }: DayFormProps) => {
 			<div className="flex gap-1 items-center w-full">
 				<div className="w-full px-1 flex flex-wrap  items-center justify-between rounded-lg border bg-card">
 					<div className="flex w-full min-w-48 max-w-48 items-center ">
-						<SwitchForm form={form} name={`enabled`} label={weekDayLabel} />
+						<SwitchForm
+							form={form}
+							name={`enabled`}
+							label={weekDayLabel}
+						/>
 						<AddNewIntervalForm availabilityId={day.id} />
 					</div>
 
 					<TimeInterval
 						intervals={day.intervals}
 						dayIndex={day.weekDay}
-						disabled={!form.getValues("enabled")}
+						disabled={!form.getValues('enabled')}
 					/>
 				</div>
 				<Button
