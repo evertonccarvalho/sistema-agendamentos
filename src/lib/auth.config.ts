2b9e7b263f9ee5677eef5cb33eb6d5d4acdd0b3a
evertonccarvalho <evertonsnake@gmail.com>
2025-01-19 01:07:52 -0300

    feat: remover configuração do Lefthook e adicionar suporte ao provedor Github no NextAuth

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -3,13 +3,17 @@ import { LoginSchema } from "@/lib/schemas";
 import bcrypt from "bcrypt";
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-
 export default {
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
 
     Credentials({
